d305c68b84894413b9fe7314b8bdc7fec507e28f
Sylvie-B <unsolo@hotmail.fr>
2021-01-19 10:29:54 +0100

    conditions composees avancees

--- if-compose-avance.php
+++ if-compose-avance.php
@@ -14,7 +14,7 @@ echo "<br>";
 echo "Mon chat est vivant, de couleur blanc et a les yeux Vert";
 echo "<br>";
 
-if(...) {
+if($animal === "Chat" && $vivant === true && $couleur === "Blanc" && $yeux === "Vert") {
     echo "Oui";
 }
 else {
@@ -39,7 +39,7 @@ $couleur = "Marron";
 $yeux = "Vert";
 
 
-if(...) {
+if($animal === "Chat" || $animal === "Chien" && $vivant === true && $couleur === "Blanc" && $yeux === "Vert") {
     echo 'Oui';
 }
 else {
@@ -60,7 +60,7 @@ $vivant = true;
 $couleur = "Blanc";
 $yeux = "Vert";
 
-if(...) {
+if($animal === "Chat" && $vivant === true && ($couleur === "Marron" || $couleur === "blanche") && $yeux === "Verts") {
     echo 'Oui';
 }
 else {
@@ -82,7 +82,7 @@ $couleur = "Noir";
 $yeux = "Vert";
 
 /** remplacer le chiffre 0 par les bonnes conditions **/
-if(...) {
+if($yeux === "Bleus" && (($animal === "Chien" && ($couleur === "Noir" || $couleur === "Marron")) || ($animal === "Chat" && $couleur === "Orange"))) {
     echo 'Oui';
 }
 else {
